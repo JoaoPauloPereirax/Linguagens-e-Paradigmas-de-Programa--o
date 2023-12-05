bcf60a16c330a5c3e84c28b2e0723f5f08341df1
João Paulo Pereira <joaopaulop.dev@gmail.com>
2023-12-05 16:23:17 -0300

    Introdução a estrutura de dados

--- Estrutura de dados com JAVA/Pilha.java
+++ Estrutura de dados com JAVA/Pilha.java
@@ -1,3 +1,18 @@
+//Bibliotecas
+import java.util.Stack;//Exclusiva para trabalhar com pilhas
+//Sistema LIFO 
 public class Pilha {
-       
+       public static void main(String[] args) {
+              Stack<String> livros = new Stack<String>();
+              System.out.println("Colocando livros na pilha...");
+              livros.push("Livro 1");
+              livros.push("Livro 2");
+              livros.push("Livro 3");
+              System.out.println("Livros na pilha: "+livros);
+              System.out.println("Retirando livros na pilha...");
+              livros.pop();
+              System.out.println("Colocando livro na pilha...");
+              livros.push("Livro 4");
+              System.out.println("Livros na pilha: "+livros);
+       }
 }
